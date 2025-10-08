c1a53a3735bb830af92b1e0b2da3552bbb8e079e
DongKey <dongkeyphe@gmail.com>
2025-10-08 14:22:37 +0700

    update base ui

--- lib/Route/app_page.dart
+++ lib/Route/app_page.dart
@@ -1,5 +1,8 @@
 import 'package:get/get.dart';
+import 'package:utshopadmin/View/Home/home.dart';
 import 'package:utshopadmin/View/Login/login.dart';
+import 'package:utshopadmin/View/Notify/notify.dart';
+import 'package:utshopadmin/View/Profile/profile.dart';
 import 'package:utshopadmin/View/dashboard.dart';
 import 'package:utshopadmin/View/splash.dart';
 
@@ -14,5 +17,8 @@ class AppPage {
     GetPage(name: Routes.splash, page: () => Splash()),
     GetPage(name: Routes.login, page: () => Login()),
     GetPage(name: Routes.dashboard, page: () => Dashboard()),
+    GetPage(name: Routes.home, page: () => Home()),
+    GetPage(name: Routes.notify, page: () => Notify()),
+    GetPage(name: Routes.profile, page: () => Profile()),
   ];
 }
--- lib/Route/app_route.dart
+++ lib/Route/app_route.dart
@@ -5,6 +5,9 @@ abstract class Routes {
   static const splash = _Paths.splash;
   static const login = _Paths.login;
   static const dashboard = _Paths.dashboard;
+  static const home = _Paths.home;
+  static const notify = _Paths.notify;
+  static const profile = _Paths.profile;
 }
 
 abstract class _Paths {
@@ -12,4 +15,7 @@ abstract class _Paths {
   static const String splash = '/splash';
   static const String login = '/login';
   static const String dashboard = '/dashboard';
+  static const String home = '/home';
+  static const String notify = '/notify';
+  static const String profile = '/profile';
 }
